d3c6daf32e8937c601b0cbfac53db96e2c49ebca
Raphael Goulart <74978784+raphasg-rp@users.noreply.github.com>
2022-11-16 15:12:35 -0300

    Add files via upload
    Signed-off-by: Raphael Goulart <74978784+raphasg-rp@users.noreply.github.com>

--- drawww.js
+++ drawww.js
@@ -1,3 +1,37 @@
+const principalContainer = document.querySelector('.principal-container');
+
+let doubles = 8;
+let contador = 1
+
+while(doubles >= 1){
+
+
+    let container = document.createElement(`div`);
+    principalContainer.appendChild(container);
+    container.classList.add('container-chave');
+
+
+    for (let i = 1; i < (doubles+1); i++) {
+        let containerChave = document.querySelectorAll('.container-chave');
+        let container1 = document.createElement('div')
+        console.log(containerChave)
+        containerChave[contador].append(container1);
+        container1.classList.add('container-dupla-1')
+        //console.log('i',i);
+
+    }
+    console.log('doubles',doubles);
+    doubles /= 2;
+    contador += 1
+}
+
+
+
+
+
+
+
+/*
 const square = document.createElement('div');
 square.classList.add('square');
 square.innerText = 'Padre Marcelo Rossi \n Paulo Cavalcante Muzy';
@@ -6,7 +40,7 @@ let insercao = document.querySelector('.testegrande');
 //insercao.insertAdjacentHTML('afterbegin', 'square');
 insercao.appendChild(square);
 
-let doubles = 8;
+
 let nameDoubles = 'teste da silva \n testando soares';
 
 // Criação dos demais retângulos utilizando a variável com o número de duplas
@@ -17,4 +51,4 @@ for (let i = 1; i < ((doubles * 2) - 2); i++) {
     clone.innerText = nameDoubles;
     //document.body.appendChild(clone);
     insercao.appendChild(clone);
-}
+} */
