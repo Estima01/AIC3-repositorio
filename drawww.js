b0731f47e64ea603bf332b8a57ecb4e297e6312c
Alan Dorneles <alanricardo.97@gmail.com>
2022-11-16 18:37:48 -0300

    commit

--- drawww.js
+++ drawww.js
@@ -1,54 +1,18 @@
-const principalContainer = document.querySelector('.principal-container');
-
 let doubles = 8;
-let contador = 1
+while (doubles >= 1) {
+  let container = document.createElement(`div`);
+  principalContainer.appendChild(container);
+  container.classList.add("container-chave");
 
-while(doubles >= 1){
-
-
-    let container = document.createElement(`div`);
-    principalContainer.appendChild(container);
-    container.classList.add('container-chave');
-
-
-    for (let i = 1; i < (doubles+1); i++) {
-        let containerChave = document.querySelectorAll('.container-chave');
-        let container1 = document.createElement('div')
-        console.log(containerChave)
-        containerChave[contador].append(container1);
-        container1.classList.add('container-dupla-1')
-        //console.log('i',i);
-
-    }
-    console.log('doubles',doubles);
-    doubles /= 2;
-    contador += 1
+  for (let i = 1; i < doubles + 1; i++){
+    let containerChave = document.querySelector(".container-chave");
+    let container1 = document.createElement("div");
+    console.log(containerChave);
+    containerChave[contador].append(container1);
+    container1.classList.add("container-dupla-1");
+    //console.log('i',i);
+  }
+  console.log("doubles", doubles);
+  doubles /= 2;
+  contador += 1;
 }
-
-
-
-
-
-
-
-/*
-const square = document.createElement('div');
-square.classList.add('square');
-square.innerText = 'Padre Marcelo Rossi \n Paulo Cavalcante Muzy';
-
-let insercao = document.querySelector('.testegrande');
-//insercao.insertAdjacentHTML('afterbegin', 'square');
-insercao.appendChild(square);
-
-
-let nameDoubles = 'teste da silva \n testando soares';
-
-// Criação dos demais retângulos utilizando a variável com o número de duplas
-// e nome dos jogadores da mesma
-
-for (let i = 1; i < ((doubles * 2) - 2); i++) {
-    const clone = square.cloneNode();
-    clone.innerText = nameDoubles;
-    //document.body.appendChild(clone);
-    insercao.appendChild(clone);
-} */
--- telas_padel/admin/campeonato/configura_partidas/script.js
+++ telas_padel/admin/campeonato/configura_partidas/script.js
@@ -1,11 +1,42 @@
-const dupla = document.querySelector('.dupla');
-dupla.addEventListener('dragstart', drag_dupla)
-function drag_dupla(e){
-  console.log('dragou')
+const columns = document.querySelectorAll(".column");
+const colunaArray = Array.from(columns)
+const tabela = document.getElementsByTagName('tbody');
+const linha = document.getElementsByClassName('column')
+document.addEventListener("dragstart",(e) =>{
+  e.target.classList.add("dragging");
 
-}
-dupla.addEventListener('dragover', drop_dupla)
-function drop_dupla(e){
-  console.log('dropou')
+ } 
+);
+document.addEventListener("dragend", (e) => {
+  e.target.classList.remove("dragging");
+});
 
-}
+columns.forEach((item)=>{
+  item.addEventListener('dragover' ,() =>{
+    /*
+    const dragging = document.querySelector(".dragging");
+    const applyAfter = getNewPosition( item, e.clientY);
+
+    if(applyAfter){
+      applyAfter.insertAdjacentElement('afterend', dragging);
+    }else{
+      item.prepend(dragging);
+    }*/
+  });
+});
+
+/*
+function getNewPosition(column, posY){
+  const cards = column.querySelectorAll(".dupla:not(.dragging)");
+  let result;
+  for(let refer_card of cards){
+    const box = refer_card.getBoundingClientRect()
+    const boxCenterY = box.x + box.height/2;
+  
+
+    if(posY >= boxCenterY) result = refer_card; 
+    
+  }
+
+  return result 
+}*/
